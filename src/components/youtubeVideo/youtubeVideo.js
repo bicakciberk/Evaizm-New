c69dd7a3fbd48a2f93997aeea20c93482afd64b0
Berk Bıçakcı <120296952+bicakciberk@users.noreply.github.com>
2024-07-19 22:19:17 +0300

    i deleted unnecessary codes

--- src/components/youtubeVideo/youtubeVideo.js
+++ src/components/youtubeVideo/youtubeVideo.js
@@ -1,25 +1,5 @@
-import axios from "axios";
-import { useState, useEffect } from "react";
 
 function YoutubeVideo() {
-  // const apiKey = "AIzaSyC-TObT_98eZm4w12dkoG_UmTcvpYksT3s";
-  // const playlistId = "PLdMS5xjrA_DUKkZgaJilMMoF5_bGTewq-";
-
-  const [url, setUrl] = useState("");
-
-  // const getData = () => {
-  //   axios
-  //     .get(
-  //       `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${playlistId}&maxResults=1&key=${apiKey}`
-  //     )
-  //     .then((data) => {
-  //       setUrl(data.data.items[0].snippet.resourceId.videoId);
-  //     });
-  // };
-
-  // useEffect(() => {
-  //   getData();
-  // }, []);
   return (
     <div className="youtubeVideo flex items-center justify-center h-screen 900px:px-[32px] 600px:px-[16px] ">
       <iframe
